45a95236e0830eef38788e91d70a93bcfb4d49dc
Agustin Ivanoff <agus.svp@gmail.com>
2023-11-02 17:09:46 -0300

    2

--- admin/secciones/servicios/crear.php
+++ admin/secciones/servicios/crear.php
@@ -1,14 +1,39 @@
-<?php include('../../templates/header.php');?>
+<?php include('../../templates/header.php'); ?>
 
 <div class="card">
     <div class="card-header">
         Crear Servicios
     </div>
     <div class="card-body">
-        <form action="" enctype="multipart/form-data" method="post"></form>
+        <form action="" enctype="multipart/form-data" method="post">
+
+            <div class="mb-3">
+                <label for="icono" class="form-label">Icono:</label>
+                <input type="text" class="form-control" name="icono" id="icono" aria-describedby="helpId" placeholder="Icono">
+
+            </div>
+
+            <div class="mb-3">
+                <label for="titulo" class="form-label">Titulo: </label>
+                <input type="text" class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Titulo">
+
+
+                <div class="mb-3">
+                    <label for="descripcion" class="form-label">Descripcion: </label>
+                    <input type="text" class="form-control" name="descripcion" id="descripcion" aria-describedby="helpId" placeholder="Descripcion">
+
+                </div>
+
+<button type="submit" class="btn btn-success">Agregar</button>
+<a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
     </div>
 
+    <div class="card-footer text-muted">
+
     </div>
+
+</div>
 </div>
 
-<?php include('../../templates/footer.php');?>
+<?php include('../../templates/footer.php'); ?>
